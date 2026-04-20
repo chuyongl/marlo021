8cca6a17f72779afc916f48dd08888e4f82e1905
chuyongl <liubpr@gmail.com>
2026-04-20 16:48:51 -0700

    feat: landing page as React route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Landing } from './pages/Landing'
 import { Signup } from './pages/Signup'
 import { Setup } from './pages/Setup'
 
@@ -6,9 +7,9 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/setup" element={<Setup />} />
-        <Route path="/" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   )
