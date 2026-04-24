d7604cf5c0d15626f88cd4e44abb59a8521a6e19
chuyongl <liubpr@gmail.com>
2026-04-23 17:46:56 -0700

    fix: alias BlogPost import to PascalCase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { Landing } from './pages/Landing'
 import { Signup } from './pages/Signup'
 import { Setup } from './pages/Setup'
 import { Help } from './pages/Help'
+import { Blog } from './pages/Blog'
+import { BlogPost_HowMarloThinks as HowMarloThinks } from './pages/BlogPost_HowMarloThinks'
 
 export default function App() {
   return (
@@ -12,7 +14,8 @@ export default function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/setup" element={<Setup />} />
         <Route path="/help" element={<Help />} />
-        <Route path="/help/*" element={<Help />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/how-marlo-thinks" element={<HowMarloThinks />} />
       </Routes>
     </BrowserRouter>
   )
